b7d6123ff335e3f9bc9647593fc5f87886ec05b1
danielfcruz2@misena.edu.co <danfcruz2@misena.edu.co>
2022-05-14 10:02:54 -0500

    cambios actualizados daniel 10:00 a.m.

--- app/src/main/java/com/example/namuiwan/erikadaniel/Familiaed.java
+++ app/src/main/java/com/example/namuiwan/erikadaniel/Familiaed.java
@@ -9,11 +9,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.namuiwan.R;
-public class Familiaed extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView btnabuela, btnabuelo, btnhija, btnhijo, btnmama, btnpapa;
+public class Familiaed extends AppCompatActivity implements View.OnClickListener{
+
+
+    ImageView  imgabuela, imgabuelo, imgpapa, imgmama, imgnina, imgnino;
     TextView txfamiliar, txparentesco;
     MediaPlayer abuela, abuelo, papa, mama, hijo, hija;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +26,19 @@ public class Familiaed extends AppCompatActivity implements View.OnClickListener
     }
 
     private void referenciar(){
-        txfamiliar = findViewById(R.id.txfamiliar);
-        txparentesco = findViewById(R.id.txparentesco);
-        btnabuela = findViewById(R.id.btnabuela);
-        btnabuela.setOnClickListener(this);
-        btnabuelo = findViewById(R.id.btnabuelo);
-        btnabuelo.setOnClickListener(this);
-        btnhija = findViewById(R.id.btnhija);
-        btnhija.setOnClickListener(this);
-        btnhijo = findViewById(R.id.btnhijo);
-        btnhijo.setOnClickListener(this);
-        btnmama = findViewById(R.id.btnmama);
-        btnmama.setOnClickListener(this);
-        btnpapa = findViewById(R.id.btnpapa);
-        btnpapa.setOnClickListener(this);
-        btnmama.setOnClickListener(this);
+
+        imgabuela = findViewById(R.id.imgabuela);
+        imgabuela.setOnClickListener(this);
+        imgabuelo = findViewById(R.id.imgabuelo);
+        imgabuelo.setOnClickListener(this);
+        imgpapa = findViewById(R.id.imgpapa;
+        imgpapa.setOnClickListener(this);
+        imgmama = findViewById(R.id.imgmama;
+        imgmama.setOnClickListener(this);
+        imgnino = findViewById(R.id.imgnino);
+        imgnino.setOnClickListener(this);
+        imgnina = findViewById(R.id.nina);
+        imgnina.setOnClickListener(this);
 
         //almacenamiento de sonidos en variables
         abuela= MediaPlayer.create(this,R.raw.abuelaed);
@@ -50,45 +51,11 @@ public class Familiaed extends AppCompatActivity implements View.OnClickListener
 
     }
 
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btnabuela:
-                txfamiliar.setText("Abuela");
-                txparentesco.setText("shura");
-                abuela.start();
-                break;
-
-            case R.id.btnabuelo:
-                txfamiliar.setText("Abuelo");
-                txparentesco.setText("shur");
-                abuelo.start();
-                break;
-
-            case R.id.btnhija:
-                txfamiliar.setText("Hija");
-                txparentesco.setText("nimpasr");
-                hija.start();
-                break;
-
-            case R.id.btnhijo:
-                txfamiliar.setText("Hijo");
-                txparentesco.setText("nusrkai");
-                hijo.start();
-                break;
-
-            case R.id.btnmama:
-                txfamiliar.setText("Mamá");
-                txparentesco.setText("Usri");
-                mama.start();
-                break;
-
-            case R.id.btnpapa:
-                txfamiliar.setText("Papá");
-                txparentesco.setText("mϴskai");
-                papa.start();
-                break;
-
+            case.R.id.imgabuela:
 
         }
 
