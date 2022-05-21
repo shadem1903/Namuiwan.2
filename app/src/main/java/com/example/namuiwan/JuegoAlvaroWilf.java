2577abade2c4b1654ed332a11a9ea49a6e0e59c2
ajmorales111@misena.edu.co <erimaca@misena.edu.co>
2022-05-21 08:12:08 -0500

    cambios para realizar pull alvaro

---
+++ app/src/main/java/com/example/namuiwan/JuegoAlvaroWilf.java
@@ -0,0 +1,14 @@
+package com.example.namuiwan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JuegoAlvaroWilf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_alvaro_wilf);
+    }
+}
