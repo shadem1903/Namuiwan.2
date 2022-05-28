a625e551aa185b0d422787fa9641d14a52cec650
dancru89 <danfcruz2@misena.edu.co>
2022-05-27 22:18:34 -0500

    cambios realizados sobre layout bosque, caa, eliminacion de algunos audios e imagenes dancru

--- app/src/main/java/com/example/namuiwan/erikadaniel/Bosqueed.java
+++ app/src/main/java/com/example/namuiwan/erikadaniel/Bosqueed.java
@@ -12,9 +12,9 @@ import com.example.namuiwan.R;
 
 public class Bosqueed extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView imgaliso,imgcastano,imgchilco,imgsalvia;
-    TextView txtaliso,txcastano,txchilco,txsalvia;
-    MediaPlayer aliso,castano,chilco,salvia;
+    ImageView imgflores, imgflores1, imguchuva, imgrosa, imgsuculenta, imgaliso, imgchilco, imgsalvia, imgbtnflores, imgbtnrosa, imgbtnsuculenta, imgbtnaliso, imgbtnchilco, imgbtnsalvia, imgbtnuchuva;
+    MediaPlayer aliso,chilco,salvia, uchuva, rosa, suculenta,flores ;
+    int valor=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,31 +25,48 @@ public class Bosqueed extends AppCompatActivity implements View.OnClickListener 
 
 
     private void referenciar(){
+
+        imgflores= findViewById(R.id.imgflores);
+        imgflores.setOnClickListener(this);
+        imgflores1= findViewById(R.id.imgflores1);
+        imgflores1.setOnClickListener(this);
+        imguchuva= findViewById(R.id.imguchuva);
+        imguchuva.setOnClickListener(this);
+        imgrosa= findViewById(R.id.imgrosa);
+        imgrosa.setOnClickListener(this);
+        imgsuculenta= findViewById(R.id.imgsuculenta);
+        imgsuculenta.setOnClickListener(this);
         imgaliso= findViewById(R.id.imgaliso);
         imgaliso.setOnClickListener(this);
-
-        imgcastano= findViewById(R.id.imgcastano);
-        imgcastano.setOnClickListener(this);
-
         imgchilco= findViewById(R.id.imgchilco);
         imgchilco.setOnClickListener(this);
-
         imgsalvia= findViewById(R.id.imgsalvia);
         imgsalvia.setOnClickListener(this);
 
-        txtaliso= findViewById(R.id.txtaliso);
-        txcastano= findViewById(R.id.txcastano);
-        txchilco= findViewById(R.id.txchilco);
-        txsalvia= findViewById(R.id.txsalvia);
+        imgbtnflores= findViewById(R.id.imgbtnflores);
+        imgbtnflores.setOnClickListener(this);
+        imgbtnuchuva= findViewById(R.id.imgbtnuchuva);
+        imgbtnuchuva.setOnClickListener(this);
+        imgbtnrosa= findViewById(R.id.imgbtnrosa);
+        imgbtnrosa.setOnClickListener(this);
+        imgbtnsuculenta= findViewById(R.id.imgbtnsuculenta);
+        imgbtnsuculenta.setOnClickListener(this);
+        imgbtnaliso= findViewById(R.id.imgbtnaliso);
+        imgbtnaliso.setOnClickListener(this);
+        imgbtnchilco= findViewById(R.id.imgbtnchilco);
+        imgbtnchilco.setOnClickListener(this);
+        imgbtnsalvia= findViewById(R.id.imgbtnsalvia);
+        imgbtnsalvia.setOnClickListener(this);
 
         //almacenar sonido en la variables
 
         aliso= MediaPlayer.create(this,R.raw.alisoed);
-        castano=MediaPlayer.create(this,R.raw.castanoed);
         chilco= MediaPlayer.create(this,R.raw.chilcoed);
         salvia= MediaPlayer.create(this,R.raw.salviaed);
-
-
+        uchuva= MediaPlayer.create(this,R.raw.uchuvaed);
+        rosa= MediaPlayer.create(this,R.raw.rosaed);
+        suculenta = MediaPlayer.create(this,R.raw.suculentased);
+        flores= MediaPlayer.create(this,R.raw.flored);
 
     }
 
@@ -57,25 +74,129 @@ public class Bosqueed extends AppCompatActivity implements View.OnClickListener 
     public void onClick(View view) {
         switch (view.getId()){
 
+            case R.id.imgflores:
+                switch (valor){
+                    case 1:
+                        imgflores.setImageResource(R.drawable.floresed4g);
+                        imgbtnflores.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgflores.setImageResource(R.drawable.floresed4);
+                        imgbtnflores.setVisibility(View.VISIBLE);
+                        flores.start();
+                        valor = 1;
+                        break;
+                }
+                break;
+
+            case R.id.imgflores1:
+                switch (valor){
+                    case 1:
+                        imgflores1.setImageResource(R.drawable.floresed1g);
+                        imgbtnflores.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgflores1.setImageResource(R.drawable.floresed1);
+                        imgbtnflores.setVisibility(View.VISIBLE);
+                        flores.start();
+                        valor = 1;
+                        break;
+                }
+                break;
+            case R.id.imguchuva:
+                switch (valor){
+                    case 1:
+                        imguchuva.setImageResource(R.drawable.uchuvaedg);
+                        imgbtnuchuva.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imguchuva.setImageResource(R.drawable.uchuvaed);
+                        imgbtnuchuva.setVisibility(View.VISIBLE);
+                        uchuva.start();
+                        valor = 1;
+                        break;
+                }
+                break;
+            case R.id.imgrosa:
+                switch (valor){
+                    case 1:
+                        imgrosa.setImageResource(R.drawable.rosaedg);
+                        imgbtnrosa.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgrosa.setImageResource(R.drawable.rosaed);
+                        imgbtnrosa.setVisibility(View.VISIBLE);
+                        rosa.start();
+                        valor = 1;
+                        break;
+                }
+                break;
+            case R.id.imgsuculenta:
+                switch (valor){
+                    case 1:
+                        imgsuculenta.setImageResource(R.drawable.suculentasedg);
+                        imgbtnsuculenta.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgsuculenta.setImageResource(R.drawable.suculentased);
+                        imgbtnsuculenta.setVisibility(View.VISIBLE);
+                        suculenta.start();
+                        valor = 1;
+                        break;
+                }
+                break;
             case R.id.imgaliso:
-                txtaliso.setText("pintsu");
-                aliso.start();
+                switch (valor){
+                    case 1:
+                        imgaliso.setImageResource(R.drawable.alisoedg);
+                        imgbtnaliso.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgaliso.setImageResource(R.drawable.alisoed);
+                        imgbtnaliso.setVisibility(View.VISIBLE);
+                        aliso.start();
+                        valor = 1;
+                        break;
+                }
                 break;
-
-            case R.id.imgcastano:
-                txcastano.setText("pikϴtsutsik");
-                castano.start();
-                break;
-
             case R.id.imgchilco:
-                txchilco.setText("kϴshak");
-                chilco.start();
+                switch (valor){
+                    case 1:
+                        imgchilco.setImageResource(R.drawable.chilcoedg);
+                        imgbtnchilco.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgchilco.setImageResource(R.drawable.chilcoed1);
+                        imgbtnchilco.setVisibility(View.VISIBLE);
+                        chilco.start();
+                        valor = 1;
+                        break;
+                }
+                break;
+            case R.id.imgsalvia:
+                switch (valor){
+                    case 1:
+                        imgsalvia.setImageResource(R.drawable.salviaedg);
+                        imgbtnsalvia.setVisibility(View.INVISIBLE);
+                        valor = 0;
+                        break;
+                    case 0:
+                        imgsalvia.setImageResource(R.drawable.salviaed);
+                        imgbtnsalvia.setVisibility(View.VISIBLE);
+                        salvia.start();
+                        valor = 1;
+                        break;
+                }
                 break;
 
-            case R.id.imgsalvia:
-                txsalvia.setText("Isϴram");
-                salvia.start();
-                break;
+
 
 
         }
