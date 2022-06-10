16bc06f94146ec12083c95a6000b5935eab4f52c
urbano4725 <efurbano@misena.edu.co>
2022-06-10 17:31:37 -0500

    cambios en yatul herramientas,frutas,verduras,calido y frio para layout-land

--- app/src/main/java/com/example/namuiwan/yatulve/Frio.java
+++ app/src/main/java/com/example/namuiwan/yatulve/Frio.java
@@ -12,8 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.namuiwan.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Frio extends AppCompatActivity implements View.OnClickListener {
-    ImageView imgUlluco,imgPapaColorada,imgPapaAmarilla,imgRepollo, imgPregunta;
+    ImageView imgUlluco,imgPapaColorada,imgPapaAmarilla,imgRepollo, imgPregunta,imgtitlerepollo,imgtitlepapaamarilla,imgtitlepapacolorada,imgtitleullucos;
     MediaPlayer ulluco,papacolorada,papaamarilla,repollo,pregunta1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,17 @@ public class Frio extends AppCompatActivity implements View.OnClickListener {
         imgPapaAmarilla.setOnClickListener(this);
         imgRepollo    = findViewById(R.id.imgRepollo);
         imgRepollo.setOnClickListener(this);
+        imgtitlerepollo = findViewById(R.id.imgtitlerepollo);
+        imgtitlerepollo.setOnClickListener(this);
+        imgtitlepapaamarilla = findViewById(R.id.imgtitlepapaamarilla);
+        imgtitlepapaamarilla.setOnClickListener(this);
+        imgtitlepapacolorada = findViewById(R.id.imgtitlepapacolorada);
+        imgtitlepapacolorada.setOnClickListener(this);
+        imgtitleullucos = findViewById(R.id.imgtitleullucos);
+        imgtitleullucos.setOnClickListener(this);
+
+
+
         //almacena sonido en la variable gallina
         ulluco    = MediaPlayer.create(this,R.raw.lau);
         papacolorada  = MediaPlayer.create(this,R.raw.pikoye);
@@ -51,16 +65,57 @@ public class Frio extends AppCompatActivity implements View.OnClickListener {
                 break;
 
             case R.id.imgUlluco:
+                imgtitleullucos.setVisibility(View.VISIBLE);
                 ulluco.start();
+                TimerTask esperar1 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitleullucos.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer1 = new Timer();
+                timer1.schedule(esperar1,1400);
+
                 break;
             case R.id.imgPapaColorada:
+                imgtitlepapacolorada.setVisibility(View.VISIBLE);
                 papacolorada.start();
+                TimerTask esperar2 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitlepapacolorada.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer2 = new Timer();
+                timer2.schedule(esperar2,1400);
+
                 break;
             case R.id.imgPapaAmarilla:
+                imgtitlepapaamarilla.setVisibility(View.VISIBLE);
                 papaamarilla.start();
+                TimerTask esperar3 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitlepapaamarilla.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer3 = new Timer();
+                timer3.schedule(esperar3,1400);
+
                 break;
             case R.id.imgRepollo:
+                imgtitlerepollo.setVisibility(View.VISIBLE);
                 repollo.start();
+
+                TimerTask esperar4 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitlerepollo.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer4 = new Timer();
+                timer4.schedule(esperar4,1400);
+
                 break;
         }
     }
--- app/src/main/java/com/example/namuiwan/yatulve/Verduras.java
+++ app/src/main/java/com/example/namuiwan/yatulve/Verduras.java
@@ -11,8 +11,12 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.namuiwan.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Verduras extends AppCompatActivity implements View.OnClickListener{
-    ImageView imgCebolla,imgZanahoria,imgTomate,imgPimiento, imgPregunta;
+    ImageView imgCebolla,imgZanahoria,imgTomate,imgPimiento, imgPregunta,imgtitlecebolla,imgtitlezanahoria,imgtitlepimiento,imgtitletomate;
     MediaPlayer cebolla,zanahoria,tomate,pimiento,pregunta3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,14 @@ public class Verduras extends AppCompatActivity implements View.OnClickListener{
         imgPimiento.setOnClickListener(this);
         imgPregunta = findViewById(R.id.imgPregunta);
         imgPregunta.setOnClickListener(this);
+        imgtitlecebolla = findViewById(R.id.imgtitlecebolla);
+        imgtitlecebolla.setOnClickListener(this);
+        imgtitlezanahoria = findViewById(R.id.imgtitlezanahoria);
+        imgtitlezanahoria.setOnClickListener(this);
+        imgtitlepimiento = findViewById(R.id.imgtitlepimiento);
+        imgtitlepimiento.setOnClickListener(this);
+        imgtitletomate = findViewById(R.id.imgtitletomate);
+        imgtitletomate.setOnClickListener(this);
 
         //almacena sonido en las variable
         cebolla    = MediaPlayer.create(this,R.raw.cebollave);
@@ -52,17 +64,57 @@ public class Verduras extends AppCompatActivity implements View.OnClickListener{
                 break;
 
             case R.id.imgCebolla:
-
+                imgtitlecebolla.setVisibility(View.VISIBLE);
                 cebolla.start();
+                TimerTask esperar1 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitlecebolla.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer1 = new Timer();
+                timer1.schedule(esperar1,1400);
+
                 break;
             case R.id.imgZanahoria:
+                imgtitlezanahoria.setVisibility(View.VISIBLE);
                 zanahoria.start();
+                TimerTask esperar2 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitlezanahoria.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer2 = new Timer();
+                timer2.schedule(esperar2,1400);
+
                 break;
             case R.id.imgTomate:
+                imgtitletomate.setVisibility(View.VISIBLE);
                 tomate.start();
+                TimerTask esperar3 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitletomate.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer3 = new Timer();
+                timer3.schedule(esperar3,1400);
+
                 break;
             case R.id.imgPimiento:
+                imgtitlepimiento.setVisibility(View.VISIBLE);
                 pimiento.start();
+
+                TimerTask esperar4 = new TimerTask() {
+                    @Override
+                    public void run() {
+                        imgtitlepimiento.setVisibility(View.INVISIBLE);
+                    }
+                };
+                Timer timer4 = new Timer();
+                timer4.schedule(esperar4,1400);
+
                 break;
         }
     }
